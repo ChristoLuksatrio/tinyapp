6f2a43ceacf1d4c4d6e6786d8e5c20fe60a55f8d
Chris Luksatrio <ranggaluksatrio@gmail.com>
2020-01-20 20:17:21 +0000

    tinyapp update

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -11,22 +13,21 @@ app.get('/', (req,res) => {
   res.send('Hello');
 })
 
+app.get("/hello", (req, res) => {
+  let templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+});
+
 app.get('/urls', (req, res) => {
-  res.json(urlDatabase);
+  let templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars);
 })
 
-app.get('/hello', (req, res) => {
-  res.send(`<html><body>Hello <b>World</b></body></html>\n`)
+app.get('/urls/:shortURL', (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
+  res.render('urls_show', templateVars);
 })
 
-app.get('/set', (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-})
-
-app.get('/fetch', (req, res) => {
-  res.send(`a = ${a}`);
-})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on PORT: ${PORT}`);
